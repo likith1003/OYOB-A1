65a7c75bd12d4bf5c68bce2397e9dc1b408c0512
likith1003 <123715227+likith1003@users.noreply.github.com>
2024-10-26 19:06:35 +0530

    Add files via upload

--- oyob/renter/models.py
+++ oyob/renter/models.py
@@ -1,1 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Brand(models.Model):
+    company = models.CharField(max_length=50, primary_key=True)
+
+    def __str__(self):
+        return self.company
+    
+
+class Bike(models.Model):
+    company = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    bike_name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=300)
+    photo = models.ImageField(upload_to='bike_photos')
+
+    def __str__(self):
+        return self.bike_name
+
+
+class Booking(models.Model):
+    booking_id = models.IntegerField(primary_key=True)
+    bike_name = models.ForeignKey(Bike, on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    pickup_date = models.DateField(auto_now=False, auto_now_add=False)
+    pickup_time = models.TimeField(auto_now=False, auto_now_add=False)
+    drop_date = models.DateField(auto_now=False, auto_now_add=False)
+    drop_time = models.TimeField(auto_now=False, auto_now_add=False)
